2843cad3c2a9eb743e75a167088009d4f382a880
Gavin Rohrer <gavin@rohrer.org>
2025-06-05 23:52:32 -0400

    put most translation regexes into a script

---
+++ translate.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -x
+
+FILE="${1:?'requires file'}"
+
+if [ ! -f "$FILE" ]; then
+	echo "$FILE does not exist!"
+	exit 1
+fi
+
+if [ ! -z "$(git diff "$FILE")" ]; then
+	echo "$FILE is dirty!"
+	exit 1
+fi
+
+trap 'git restore "$FILE"' SIGINT EXIT
+
+function s() {
+	: ${1:?'requires sed expression'}
+	sed -iE -e "$1" "$FILE"
+}
+
+function v() {
+	: ${1:?'requires sed expression'}
+	nvim -Es "$FILE" -c "$1" -c ":w"
+}
+
+
+
+s 's/#include.*//'
+s 's/where/where_/g'
+s 's/->/./g'
+s 's/\/\*!/\/*/'
+
+# Functions on one line
+v ':g/\v^\w+ \**\w+\([^)]+$/norm vibVJ'
+
+# Function argument syntax
+v ':g/\v^\w+/:s/\v(\w+_t) (\**)(\w+)([,)])/\3: \2\1\4/g'
+v ':g/\v^\w+/:s/\V*/*mut /g'
+
+
+# slice indexing
+v ':%s/\v\[([^=\]]*[-+*/ ][^=\]]*)\]/[(\1) as usize]/g'
+v ':%s/\v\[(\w+)\]/[\1 as usize]/g'
+v ':%s/\]\]/] as usize]/'
+
+# the rest of the function signature
+v ':%s/\v^(\w+) (%(\*mut )*)([^{]*)/#[metis_func]\rpub extern "C" fn \3 -> \2\1/'
+
+# bracketless if/for
+v ':%s/\v(^ *%(for|if) ?\([^{]*)\n(.*;)/\1 {\r\2\r}'
+v ':%s/\v(^ *else *\n([^{]*)\n(.*;)/\1 {\r\2\r}' || true
+
+
+# switch -> match
+s 's/switch/match/'
+s 's/case \(\w\+\):/\1 => {/2'
+s 's/default:/_ =>/'
+
+# comment out variable declarations
+v ':g/\v^  (int|(\w*_t)) /norm gcc'
+s 's/^ *nvtxs *= graph\.nvtxs/let & as usize/'
+s 's/^ *ncon *= graph\.ncon/let & as usize/'
+
+# for loops
+v ':%s/for (\([a-zA-Z]\+=.\w*\),\s*/\1;\rfor (/' || true
+v ':%s/for (\([a-zA-Z]\+=.\w*\),\s*/\1;\rfor (/' || true
+v ':%s/\vfor \(([a-zA-Z]+)\=(.{-1,}); \1\<(.{-1,}); \1\+\+\)/for \1 in (\2)..(\3)'
+
+# make sure for loops are gone
+! grep -qE '\s*for.*;' "$FILE"
+
+# post/pre-fix increment/decrement
+s 's/++;\s*$/ += 1;/'
+
+v ':g/++\w\+/norm f+xxyiw0pwi+=1;' || true
+v ':g/\w\+++/norm f+xxhyiw$pA+=1;' || true
+
+v ':g/--\w\+/norm f-xxyiw0pwi-=1;' || true
+v ':g/\w\+--/norm f-xxhyiw$pA-=1;' || true
+
+# printing
+v ':%s/\v\%(.{-0,5})"%(PR%(IDX|REAL))"/{:\1}/g'
+s 's/%s/{}/'
+s 's/\<printf\>/print!/'
+
+# temp allocations
+v ':%s/\viset\(([^,]+), (-?\d), iwspacemalloc\(ctrl, .+\)\);/vec![\2; \1 as usize];/g' || true
+v ':%s/\viwspacemalloc\(ctrl, (.*)\);/vec![0; \1 as usize];/g' || true
+v ':%s/\vrwspacemalloc\(ctrl, (.*)\);/vec![0.0; \1 as usize];/g' || true
+v ':g/WCORE\%\(PUSH\|POP\)/norm gcc' || true
+
+# misc
+s 's/-> void//' || true
+s 's/NULL/std::ptr::null_mut()/g' || true
+
+# cargo fix --allow-dirty --broken-code
+
+git diff "$FILE"
